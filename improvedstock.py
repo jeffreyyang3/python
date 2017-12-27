229742ec44834f38e09fe49db70c8d50ae53f184
JeffreyYang <jeffreyhasmail@gmail.com>
2017-12-27 15:45:24 +0800

    sigh

--- improvedstock.py
+++ improvedstock.py
@@ -7,9 +7,18 @@ import urllib2
 quote_page = "http://www.nasdaq.com/symbol/" + raw_input("whats symbol of stock u want learn about?? ")
 soup = BeautifulSoup(urllib2.urlopen(quote_page), "html.parser")
 #print (soup.find("h1", attrs = {"class" :"name"}).string)
+"""if (soup.find("div", attrs = {"id" : "qwidget_percent"}).find("Green", beg=0, end=len("Green")) == -1):
+	dank = "-"
+else:
+	dank = "+"
+"""
+
 print(soup.find("div", attrs = {"id" : "qwidget_pageheader" }).text + ":")
 print(soup.find("div", attrs = {"class":"qwidget-dollar"}).text)
 print(soup.find("div", attrs = {"id" : "qwidget_percent"}).text)
+
+
+
 
 
 
